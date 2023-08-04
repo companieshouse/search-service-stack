0a17a9e1ff300441cc7747bf66416b3c0d5e8fa2
fisher-e <efisher@companieshouse.gov.uk>
2023-08-04 11:49:57 +0100

    initial terraform for ecs cluster

---
+++ groups/infrastructure/main.tf
@@ -0,0 +1,94 @@
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4..54.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 3.18.0"
+    }
+  }
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = local.region
+}
+
+
+# Configure the remote state data source to acquire configuration
+# created through the code in ch-service-terraform/aws-mm-networks.
+data "terraform_remote_state" "networks" {
+  backend = "s3"
+  config = {
+    bucket = var.remote_state_bucket
+    key    = "${var.state_prefix}/${var.deploy_to}/${var.deploy_to}.tfstate"
+    region = var.aws_region
+  }
+}
+locals {
+  vpc_id            = data.terraform_remote_state.networks.outputs.vpc_id
+  application_ids   = data.terraform_remote_state.networks.outputs.application_ids
+}
+
+# Configure the remote state data source to acquire configuration
+# created through the code in the search-service-stack-configs stack in the
+# aws-common-infrastructure-terraform repo.
+data "terraform_remote_state" "search-service-stack-configs" {
+  backend = "s3"
+  config = {
+    bucket = var.aws_bucket # aws-common-infrastructure-terraform repo uses the same remote state bucket
+    key    = "aws-common-infrastructure-terraform/common-${var.aws_region}/search-service-stack-configs.tfstate"
+    region = var.aws_region
+  }
+}
+
+provider "vault" {
+  auth_login {
+    path = "auth/userpass/login/${var.vault_username}"
+    parameters = {
+      password = var.vault_password
+    }
+  }
+}
+
+data "vault_generic_secret" "secrets" {
+  path = "applications/${var.aws_profile}/${var.environment}/${local.stack_fullname}"
+}
+
+locals {
+  # stack name is hardcoded here in main.tf for this stack. It should not be overridden per env
+  stack_name     = "search-service"
+  stack_fullname = "${local.stack_name}-stack"
+  name_prefix    = "${local.stack_name}-${var.environment}"
+}
+
+module "ecs-cluster" {
+  source = "git@github.com:companieshouse/terraform-library-ecs-cluster.git?ref=1.1.4"
+
+  stack_name                 = local.stack_name
+  name_prefix                = local.name_prefix
+  environment                = var.environment
+  vpc_id                     = local.vpc_id
+  subnet_ids                 = local.application_ids
+  ec2_key_pair_name          = var.ec2_key_pair_name
+  ec2_instance_type          = var.ec2_instance_type
+  ec2_image_id               = var.ec2_image_id
+  asg_max_instance_count     = var.asg_max_instance_count
+  asg_min_instance_count     = var.asg_min_instance_count
+  enable_container_insights  = var.enable_container_insights
+  asg_desired_instance_count = var.asg_desired_instance_count
+}
+
+module "secrets" {
+  source = "./module-secrets"
+
+  stack_name  = local.stack_name
+  name_prefix = local.name_prefix
+  environment = var.environment
+  kms_key_id  = data.terraform_remote_state.search-service-stack-configs.outputs.services_stack_configs_kms_key_id
+  secrets     = data.vault_generic_secret.secrets.data
+}
