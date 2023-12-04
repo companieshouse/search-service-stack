5c1100bcef99b38ff332344fdf6d6a8e0c80c851
Ivana Pavic <ipavic@companieshouse.gov.uk>
2023-12-04 14:44:47 +0000

    CC-1034-Upgrade-search-service

--- groups/infrastructure/locals.tf
+++ groups/infrastructure/locals.tf
@@ -5,10 +5,11 @@ locals {
 
   stack_secrets = jsondecode(data.vault_generic_secret.secrets.data_json)
 
-  application_subnet_pattern = local.stack_secrets["application_subnet_pattern"]
-  application_subnet_ids     = join(",", data.aws_subnets.application.ids)
-  kms_key_alias              = local.stack_secrets["kms_key_alias"]
-  vpc_name                   = local.stack_secrets["vpc_name"]
+  application_subnet_pattern  = local.stack_secrets["application_subnet_pattern"]
+  application_subnet_ids      = join(",", data.aws_subnets.application.ids)
+  kms_key_alias               = local.stack_secrets["kms_key_alias"]
+  vpc_name                    = local.stack_secrets["vpc_name"]
+  notify_topic_slack_endpoint = local.stack_secrets["notify_topic_slack_endpoint"]
 
   parameter_store_secrets = {
     "web-oauth2-client-id"     = local.stack_secrets["web-oauth2-client-id"],
